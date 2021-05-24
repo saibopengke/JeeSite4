758d28289199daec6054f6051cd04f1df4df6be1
saibopengke <949826163@qq.com>
2021-05-24 15:58:44 +0800

    deploy_tomcat_script_pipeline.groovy

--- deploy/deploy_tomcat_script_pipeline.groovy
+++ deploy/deploy_tomcat_script_pipeline.groovy
@@ -1,6 +1,8 @@
 node('master') {
     stage('同步源码') {
-            git([url: 'git@gitee.com:11547299/jeesite4.git', branch: '${branch}'])
+        //git([url: 'git@gitee.com:11547299/jeesite4.git', branch: '${branch}'])
+        git([url: 'git@github.com:saibopengke/JeeSite4.git', branch: '${branch}'])
+
     }
 
     stage('maven编译打包') {
